264d465fa557a6ac8a9b96598b8373b668fe2d53
CofgCode <80172904+CofgCode@users.noreply.github.com>
2025-04-18 19:29:27 -0500

    fix: using names

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "aws_instance" "blog" {
   }
 }
 
-resource "aws_lb" "alb" {
+resource "aws_lb" "front_end" {
   name               = "blog-alb"
   internal           = false
   load_balancer_type = "application"
@@ -68,25 +68,23 @@ resource "aws_lb" "alb" {
   }
 }
 
-resource "aws_lb_target_group" "target_gr" {
+resource "aws_lb_target_group" "front_end" {
   name     = "instance_tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = module.blog_vpc.vpc_id
 }
 
-resource "aws_lb_listener" "listener_alb" {
-  load_balancer_arn = aws_lb.alb.arn
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.front_end.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.target_gr.arn
+    target_group_arn = aws_lb_target_group.front_end.arn
   }
 }
-
-
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
