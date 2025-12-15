13275b3bbac421710bf26985cb3cb4b2cda0f927
Cesar Forero <Cesar.octavio.forero@gmail.com>
2025-12-14 20:11:09 -0500

    adding modular changes

--- README.md
+++ README.md
@@ -11,23 +11,16 @@ Leverage code modules from the Terraform registry and how to create your own mod
 
 _See the readme file in the main branch for updated instructions and information._
 ## Instructions
-This repository has branches for each of the videos in the course. You can use the branch pop up menu in github to switch to a specific branch and take a look at 
-the course at that stage, or you can add `/tree/BRANCH_NAME` to the URL to go to the branch you want to access.
+You will need terraform cloud to deploy this proyect
+
 
 ## Branches
-The branches are structured to correspond to the videos in the course. The naming convention is `CHAPTER#_MOVIE#`. As an example, the branch named `02_03` corresponds to the second chapter and the third video in that chapter. The code is built sequentally so each branch contains the completed code for that particular video and the starting code can be found in the previous video's branch.
+There are no branches, future changes once completed they will be integrated in main.
 
-The `main` branch contains the starting code for the course and the `final` branch contains the completed code.
+### Developer
 
-### Instructor
-
-Josh Samuelson 
+Cesar Forero
                             
 DevOps Engineer
 
                             
-
-Check out my other courses on [LinkedIn Learning](https://www.linkedin.com/learning/instructors/josh-samuelson).
-
-[lil-course-url]: https://www.linkedin.com/learning/learning-terraform-15575129?dApp=59033956
-[lil-thumbnail-url]: https://cdn.lynda.com/course/3087701/3087701-1666200696363-16x9.jpg
--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,7 +11,7 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owner]
 }
 
 data "aws_vpc" "default" {
@@ -21,21 +21,41 @@ data "aws_vpc" "default" {
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = var.environment.name
+  
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  # private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  # private_subnets = ["${}.1.0/24", "${}.2.0/24", "${}.3.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
   #enable_nat_gateway = true
   #enable_vpn_gateway = true
 
   tags = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
+/*
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
+
+  name = "${var.environment.name}-asg-blog"
+
+  min_size            = var.asg_min_size
+  max_size            = var.asg_max_size
+  desired_capacity    = var.asg_min_size
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
+}
+*/
 
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
@@ -51,7 +71,7 @@ resource "aws_instance" "blog" {
 }
 
 resource "aws_lb" "front_end" {
-  name               = "blog-alb"
+  name               = "${var.environment.name}-blog-alb"
   internal           = false
   load_balancer_type = "application"
   #vpc_id          = module.blog_vpc.vpc_id
@@ -63,13 +83,13 @@ resource "aws_lb" "front_end" {
   enable_deletion_protection = false
 
   tags = {
-    Environment = "Development"
-    Project     = "Example"
+    Environment = var.environment.name
+    Project     = "Container application"
   }
 }
 
 resource "aws_lb_target_group" "front_end" {
-  name     = "lb-tg"
+  name     = "${var.environment.name}-blog-alb-tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = module.blog_vpc.vpc_id
@@ -89,7 +109,7 @@ resource "aws_lb_listener" "front_end" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
-  name = "blog_new"
+  name = "${var.environment.name}-blog"
   
   # remove public vpc
   #vpc_id              = data.aws_vpc.default.id
@@ -142,3 +162,12 @@ module "blog_sg" {
 
 #   security_group_id = aws_security_group.blog.id
 # }
+terraform {
+  cloud {
+    organization = "myiacterracloud"
+
+    workspaces {
+      name = "wsterracloud"
+    }
+  }
+}
